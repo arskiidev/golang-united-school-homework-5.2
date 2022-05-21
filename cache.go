556006abb2b5e9f5968382ffd0be78c397676de2
hypnospaced <hypnospaced@yandex.ru>
2022-05-21 16:42:30 +0400

    Homework

--- cache.go
+++ cache.go
@@ -2,22 +2,61 @@ package cache
 
 import "time"
 
+type Node struct {
+	key, value string
+	deadline   time.Time
+}
+
 type Cache struct {
+	cache []Node
 }
 
 func NewCache() Cache {
 	return Cache{}
-}
-
-func (receiver) Get(key string) (string, bool) {
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	for i, n := range c.cache {
+		if key == n.key {
+			c.cache[i].value = value
+			return
+		}
+	}
+	c.cache = append(c.cache, Node{key: key, value: value})
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	for i, n := range c.cache {
+		if key == n.key {
+			c.cache[i].value = value
+			c.cache[i].deadline = deadline
+			return
+		}
+	}
+	c.cache = append(c.cache, Node{key: key, value: value, deadline: deadline})
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) Get(key string) (string, bool) {
+	for i, n := range c.cache {
+		if key == n.key {
+			if !n.deadline.IsZero() && !time.Now().Before(n.deadline) {
+				c.cache = append(c.cache[:i], c.cache[i+1:]...)
+				return "", false
+			}
+			return n.value, true
+		}
+	}
+	return "", false
+}
+
+func (c *Cache) Keys() (s []string) {
+	for i, n := range c.cache {
+		if !n.deadline.IsZero() && !time.Now().Before(n.deadline) {
+			c.cache = append(c.cache[:i], c.cache[i+1:]...)
+			continue
+		}
+		s = append(s, n.value)
+	}
+	return s
 }
